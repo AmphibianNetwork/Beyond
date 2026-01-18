95f2ddbecae7284906682c21899233284ec4d4ae
Ramon <bonesyep@gmail.com>
2026-01-18 19:50:34 -0300

    feat(kubejs): added block for the spaceship

---
+++ modpack/kubejs/startup_scripts/custom_blocks.js
@@ -0,0 +1,7 @@
+StartupEvents.registry("block", (event) => {
+    event.create("forgotten_planks") 
+    .displayName("Forgotten Planks")
+    .material("wood") 
+    .hardness(1.0)
+    .resistance(1.0)
+})
