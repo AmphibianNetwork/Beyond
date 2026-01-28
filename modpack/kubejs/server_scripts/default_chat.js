942948d07d154d1677ec0284dc206a0faff6f6ac
Ramon <bonesyep@gmail.com>
2026-01-27 23:03:28 -0300

    feat(kubejs): made default chat on the mod itself. Only tells players about the commands with kubejs

--- modpack/kubejs/server_scripts/default_chat.js
+++ modpack/kubejs/server_scripts/default_chat.js
@@ -1,9 +1,6 @@
 PlayerEvents.loggedIn(event => {
   const player = event.player
 
-  // Run your mod's command automatically
-  player.runCommandSilent("chat global")
-
   player.sendSystemMessage(
     Text.gold("Default chat mode is Global. Do /chat local to switch.")
   )
